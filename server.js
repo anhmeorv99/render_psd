6930d837bc7b4eadb15ddfa43e2dd58438002e6a
anhmeorv99 <anhmeorv99@gmail.com>
2021-03-15 17:29:44 +0700

    add server

--- server.js
+++ server.js
@@ -2,15 +2,22 @@
  * Created by trungquandev.com's author on 23/06/2019.
  * server.js
  */
- 
+
+const imgurUploader = require("imgur-uploader");
+const toBuffer = require('typedarray-to-buffer');
+const delay = require('delay');
+
+require('ag-psd/initialize-canvas.js');
 let express = require("express");
 let multer = require("multer");
 let path = require("path");
 let bodyParser = require("body-parser");
+const fs = require("fs");
+const {readPsd} = require("ag-psd");
  
 let app = express();
 // Khai báo sử dụng body-parser để nhận data từ form
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.urlencoded({extended: true}));
  
 // Route này trả về cái form upload cho client
 app.get("/", (req, res) => {
@@ -26,14 +33,14 @@ let diskStorage = multer.diskStorage({
   filename: (req, file, callback) => {
     // ở đây các bạn có thể làm bất kỳ điều gì với cái file nhé.
     // Mình ví dụ chỉ cho phép tải lên các loại ảnh png & jpg
-    let math = ["image/png", "image/jpeg"];
-    if (math.indexOf(file.mimetype) === -1) {
-      let errorMess = `The file <strong>${file.originalname}</strong> is invalid. Only allowed to upload image jpeg or png.`;
-      return callback(errorMess, null);
-    }
- 
+    // let math = ["image/png", "image/jpeg"];
+    // if (math.indexOf(file.mimetype) === -1) {
+    //   let errorMess = `The file <strong>${file.originalname}</strong> is invalid. Only allowed to upload image jpeg or png.`;
+    //   return callback(errorMess, null);
+    // }
+    //
     // Tên của file thì mình nối thêm một cái nhãn thời gian để đảm bảo không bị trùng.
-    let filename = `${Date.now()}-trungquandev-${file.originalname}`;
+    let filename = file.originalname;
     callback(null, filename);
   }
 });
@@ -52,13 +59,20 @@ app.post("/upload", (req, res) => {
       return res.send(`Error when trying to upload: ${error}`);
     }
 
-    console.log(`------Request body-----`);
-    console.log(req.body);
+    const buffer = fs.readFileSync(req.file['path']);
+    const psd1 = readPsd(buffer, {useImageData: true});
+    const array_img = psd1.linkedFiles;
 
-    console.log(`------Request file-----`);
-    console.log(req.file);
-
-    console.log(`------Test Done-----`)
+    (async function doSomeStuff() {
+      await new Promise(resolve => setTimeout(resolve, 5000));
+      for (const item of array_img) {
+        var buff = toBuffer(item.data)
+        imgurUploader(buff).then(data => {
+          console.log({name: item.name, url: data.link})
+        });
+      }
+      await doSomeStuff();
+    })();
 
     // Không có lỗi thì lại render cái file ảnh về cho client.
     // Đồng thời file đã được lưu vào thư mục uploads
